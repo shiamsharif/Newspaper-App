65800efd1cb791c902d8e35f7cd694d8b26a0cf3
shiamsharif <shiam.sharif.07@gmail.com>
2024-07-03 18:27:14 +0600

    BUG Solved article_details

--- articles/models.py
+++ articles/models.py
@@ -17,6 +17,6 @@ class Article(models.Model):
         return self.title
     
     def get_absolute_url(self):
-        return reverse("article_details", args=[str(self.id)])
+        return reverse("article_detail", args=[str(self.id)])
     
 
