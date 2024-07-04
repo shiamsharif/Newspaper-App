2f22261527a91d3b31d9d97baf339e5709709b53
shiamsharif <shiam.sharif.07@gmail.com>
2024-07-04 10:54:30 +0600

    ADD: form validation

--- articles/views.py
+++ articles/views.py
@@ -28,4 +28,9 @@ class ArticleDeleteView(DeleteView):
 class ArticleCreateView(CreateView):
     model = Article
     template_name = 'article_new.html'
-    fields = ('title', 'body', 'author', )
+    fields = ('title', 'body')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
